2ed39ff6fef2afad332c3927389443e2f50ebde7
Jonathan Miller <jon@jmill.dev>
2022-08-02 21:19:38 +0100

    Stuff

--- bot.py
+++ bot.py
@@ -3,46 +3,48 @@ import atexit
 import json
 import logging
 import os
+from pathlib import Path
 
 import discord
 from discord import SyncWebhook
 from discord.ext import commands
 from dotenv import load_dotenv
 from marsbots import constants
-from marsbots.models import MarsBotSettings
+from marsbots.models import MarsBotMetadata
 
 
 class MarsBot(commands.Bot):
-    def __init__(self, specfile_path: str) -> None:
+    def __init__(self, metadata_path: str) -> None:
         intents = discord.Intents.default()
-        self.settings = self.load_settings(specfile_path)
+        self.metadata = self.load_metadata(metadata_path)
         self.set_intents(intents)
         self.configure_logging()
         commands.Bot.__init__(
             self,
-            command_prefix=self.settings.command_prefix,
+            command_prefix=self.metadata.command_prefix,
             intents=intents,
         )
         atexit.register(self.post_exit_webhook)
 
-    def load_settings(self, specfile_path: str) -> MarsBotSettings:
-        settings = json.load(open(specfile_path))
-        if "command_prefix" not in settings:
+    def load_metadata(self, metadata_path: str) -> MarsBotMetadata:
+        metadata = json.load(open(metadata_path))
+        if "command_prefix" not in metadata:
             # Hack to allow a bot without command prefix
-            settings["command_prefix"] = constants.UNLIKELY_PREFIX
-        if "intents" not in settings:
-            settings["intents"] = []
-        return MarsBotSettings(**settings)
+            metadata["command_prefix"] = constants.UNLIKELY_PREFIX
+        if "intents" not in metadata:
+            metadata["intents"] = []
+        return MarsBotMetadata(**metadata)
 
     def set_intents(self, intents: discord.Intents) -> None:
+        intents.message_content = True
         intents.messages = True
-        if "presence" in self.settings.intents:
+        if "presence" in self.metadata.intents:
             intents.presences = True
-        if "members" in self.settings.intents:
+        if "members" in self.metadata.intents:
             intents.members = True
 
     def configure_logging(self) -> None:
-        logdir = constants.LOG_DIR / self.settings.name
+        logdir = constants.LOG_DIR / self.metadata.name
         logfile = str(logdir / "discord.log")
         logdir.mkdir(parents=True, exist_ok=True)
 
@@ -56,8 +58,7 @@ class MarsBot(commands.Bot):
         )
 
     async def on_ready(self) -> None:
-        print(f"Running {self.settings.name}...")
-        logging.info(f"Running {self.settings.name}...")
+        print(f"Running {self.metadata.name}...")
 
     async def on_message(self, message: discord.Message) -> None:
         if message.author.bot:
@@ -70,37 +71,45 @@ class MarsBot(commands.Bot):
         if webhook_url:
             try:
                 webhook = SyncWebhook.from_url(webhook_url)
-                webhook.send(f"{self.settings.name} is down.")
+                webhook.send(f"{self.metadata.name} is down.")
             except Exception as e:
                 logging.error("Unable to post exit to webhook.")
                 logging.error(str(e))
 
 
 def start(
-    specfile_path: str,
-    cog_path: str,
+    bot_name: str,
+    metadata_path: str,
+    cog_paths: str,
     dotenv_path: str = constants.DOTENV_PATH,
 ) -> None:
 
+    bot_dir = Path(__file__).parent / "bots" / bot_name
+    metadata_path = metadata_path or bot_dir / "metadata.json"
+    cog_paths = cog_paths or [f"bots.{bot_name}.{bot_name}"]
+    dotenv_path = dotenv_path or bot_dir / ".env"
+
     print("Launching bot...")
-    logging.info("Launching bot...")
     load_dotenv(dotenv_path)
 
-    bot = MarsBot(specfile_path)
-    for path in cog_path:
-        bot.load_extension(path)
-    bot.run(os.getenv(bot.settings.token_env))
+    bot = MarsBot(metadata_path)
+    for path in cog_paths:
+        res = bot.load_extension(path)
+        if type(res[path]) != bool:
+            raise Exception(res[path])
+    bot.run(os.getenv(bot.metadata.token_env))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="MarsBot")
-    parser.add_argument("specfile", help="specfile path")
+    parser.add_argument("bot_name", help="Name of the bot to load from /bots directory")
+    parser.add_argument("--metadata_path", help="Path to a custom metadata file")
     parser.add_argument(
-        "--cog-path",
+        "--cog-paths",
         help="Path to one or more cog files to load, using python module syntax",
         nargs="+",
         default=[],
     )
     parser.add_argument("--dotenv-path", help="Path to a custom .env file")
     args = parser.parse_args()
-    start(args.specfile, args.cog_path, args.dotenv_path)
+    start(args.bot_name, args.metadata_path, args.cog_paths, args.dotenv_path)
